14c145783fd71879fcab59768b6b7c654c84afaf
Sergei Zotov <zsv.main@gmail.com>
2020-02-29 00:37:23 +0500

    feat: realize towelSort method

--- src/index.js
+++ src/index.js
@@ -2,5 +2,43 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let newArr = [];  
+  if (!matrix) return [];
+  matrix.forEach((element,i )=> {
+    if (i%2!=0) {
+      element.reverse().forEach((item)=>{
+        newArr.push(item);
+      })
+    } else {
+      element.forEach((item,i)=>{
+        newArr.push(item);
+      })
+    }    
+  });
+  return newArr;
 }
+
+
+// function towelSort(matrix) {
+//   let newArr = [];
+//   matrix.forEach((element,i )=> {
+//     if (i%2!=0) {
+//       element.reverse().forEach((item)=>{
+//         newArr.push(item);
+//       })
+//     } else {
+//       element.forEach((item,i)=>{
+//         newArr.push(item);
+//       })
+//     }    
+//   });
+//   return newArr;
+// }
+
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
+// console.log(towelSort(matrix))  
